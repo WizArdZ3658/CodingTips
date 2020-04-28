c97683baf9b619cbf8659e98f701c16d9f860203
Somnath <p.somnath2599@gmail.com>
2020-04-28 19:51:05 +0530

    Updated settings

--- website_twitter/settings.py
+++ website_twitter/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oc50=xt^bs#65e3+dh76n5!5pho+%*1h)$^hcblojjkz+s+j@+'
-
+# SECRET_KEY = "oc50=xt^bs#65e3+dh76n5!5pho+%*1h)$^hcblojjkz+s+j@+"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codingtips.herokuapp.com']
 
 
 # Application definition
