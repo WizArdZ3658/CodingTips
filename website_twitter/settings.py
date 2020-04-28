81a5d8e65dcdc51332b38f3f1fb1986a86a271d8
Somnath <p.somnath2599@gmail.com>
2020-04-28 19:59:13 +0530

    added django-heroku

--- website_twitter/settings.py
+++ website_twitter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'ap-south-1'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
