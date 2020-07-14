ceeee3a104e6522eb7b5319c5006895d91308d68
Somnath <p.somnath2599@gmail.com>
2020-07-14 16:15:20 +0530

    switched from SQLite to Postgres

--- website_twitter/settings.py
+++ website_twitter/settings.py
@@ -69,10 +69,21 @@ WSGI_APPLICATION = 'website_twitter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
